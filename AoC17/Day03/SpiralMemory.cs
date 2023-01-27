551ce9e5aafa29d82b7fe984635fa293edfd3e6b
silentDoc <ramon78t@yahoo.es>
2023-01-27 14:00:12 +0100

    Day 3 - Part 1 Completed. Spiral Access

---
+++ AoC17/Day03/SpiralMemory.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace AoC17.Day03
+{
+    internal class SpiralMemory
+    {
+        int input = 0;
+
+        public void ParseInput(List<string> lines)
+            => input = int.Parse(lines[0]);
+
+        int FindDistance(int part = 1)
+        {
+            var currentRollCorner = 1;
+            var step = 0;
+            
+            while (currentRollCorner < input)
+            {
+                step += 2;
+                currentRollCorner += 4*step;
+            }
+
+            var highLimit = currentRollCorner;
+            var lowLimit = highLimit - step - 1;
+
+            while (!(input >= lowLimit && input <= highLimit))
+            {
+                highLimit = lowLimit;
+                lowLimit = highLimit - step - 1;
+            }
+            var offset = Math.Abs( ((lowLimit + highLimit) / 2) - input);
+
+            return step/2 + offset + 1;
+        }
+
+
+        public int Solve(int part = 1)
+            => FindDistance(part);
+    }
+}
