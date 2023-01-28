5695f41772a676ed9e4b7b0239b6a5732925aad6
silentDoc <ramon78t@yahoo.es>
2023-01-28 12:45:16 +0100

    Day 3 - Part 2 Completed. Spiral with neighborhood

--- AoC17/Day03/SpiralMemory.cs
+++ AoC17/Day03/SpiralMemory.cs
@@ -1,10 +1,11 @@
 ﻿using AoC17.Common;
+using System.Diagnostics;
 
 namespace AoC17.Day03
 {
     record SpiralElement
     {
-        Coord2D position = new(0,0);
+        public Coord2D position = new(0,0);
         public int value;
     }
 
@@ -12,10 +13,15 @@ namespace AoC17.Day03
     {
         int input = 0;
 
+        readonly Coord2D DIR_UP = new Coord2D(0, -1);
+        readonly Coord2D DIR_LEFT = new Coord2D(-1, 0);
+        readonly Coord2D DIR_DOWN = new Coord2D(0, 1);
+        readonly Coord2D DIR_RIGHT = new Coord2D(1, 0);
+
         public void ParseInput(List<string> lines)
             => input = int.Parse(lines[0]);
 
-        int FindDistance(int part = 1)
+        int FindDistance()
         {
             var currentSpiralCorner = 1;
             var step = 0;
@@ -41,12 +47,64 @@ namespace AoC17.Day03
 
         int GenerateSpiral()
         {
+            List<SpiralElement> spiral = new();
 
+            Coord2D referenceTopRight = new Coord2D(1, -1);
+            Coord2D referenceTopLeft = new Coord2D(-1, -1);
+            Coord2D referenceBottomLeft = new Coord2D(-1, 1);
+            Coord2D referenceBottomRight = new Coord2D(1, 1);
 
-            return 0;
+            Coord2D cornerTopRight = new Coord2D(1, -1);
+            Coord2D cornerTopLeft = new Coord2D(-1, -1);
+            Coord2D cornerBottomLeft = new Coord2D(-1, 1);
+            Coord2D cornerBottomRight = new Coord2D(1, 1);
+
+            SpiralElement start = new SpiralElement();
+            start.value = 1;
+            var currentValue = 1;
+            var step = 1;
+
+            spiral.Add(start);
+            var currentNode = new SpiralElement();
+            currentNode.position += DIR_RIGHT;
+            currentNode.value = 1;
+            spiral.Add(currentNode);
+
+            while (currentValue < input)
+            {
+                var previousNode = currentNode;
+                currentNode = new SpiralElement();
+
+                Coord2D direction = new(0,0);
+                if (!spiral.Any(x => x.position == cornerTopRight))
+                    direction = DIR_UP;
+                else if (!spiral.Any(x => x.position == cornerTopLeft))
+                    direction = DIR_LEFT;
+                else if (!spiral.Any(x => x.position == cornerBottomLeft))
+                    direction = DIR_DOWN;
+                else if (!spiral.Any(x => x.position == cornerBottomRight))
+                    direction = DIR_RIGHT;
+                else
+                {
+                    // We covered all the nodes, the spiral grows
+                    direction = DIR_RIGHT;
+                    step++;
+                    cornerTopRight = referenceTopRight * step;
+                    cornerTopLeft = referenceTopLeft * step;
+                    cornerBottomRight = referenceBottomRight * step;
+                    cornerBottomLeft = referenceBottomLeft * step;
+                }
+
+                currentNode.position = previousNode.position + direction;
+                var neighs = currentNode.position.GetNeighbors8();
+                currentNode.value = spiral.Where(x => neighs.Contains(x.position)).Sum(s => s.value);
+                currentValue = currentNode.value;
+                spiral.Add(currentNode);
+            }
+            return currentValue;
         }
 
         public int Solve(int part = 1)
-            => FindDistance(part);
+            => (part == 1) ? FindDistance() : GenerateSpiral();
     }
 }
