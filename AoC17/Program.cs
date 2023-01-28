b7bbd48d8d1e5d29576a644e7dfe195e4933a897
silentDoc <ramon78t@yahoo.es>
2023-01-28 15:41:36 +0100

    Day 5 - Prepare Main, Readme and Input

--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -6,9 +6,9 @@ namespace AoC17
     {
         static void Main(string[] args)
         {
-            int day = 4;
-            int part = 2;
-            bool test = false;
+            int day = 5;
+            int part = 1;
+            bool test = !false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -22,6 +22,7 @@ namespace AoC17
                 2 => day2(input, part).ToString(),
                 3 => day3(input, part).ToString(),
                 4 => day4(input, part).ToString(),
+                5 => day5(input, part).ToString(),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -63,6 +64,13 @@ namespace AoC17
             passChecker.ParseInput(lines);
             
             return passChecker.Solve(part);
+        }
+
+        static int day5(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+
+            return 0;
         }
 
     }
