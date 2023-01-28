c58999e5f0cbf1528f651e3b1ac7af96bfcf79ad
silentDoc <ramon78t@yahoo.es>
2023-01-28 15:37:30 +0100

    Day 4 - Part 2 Completed. Dealing with anagrams

--- AoC17/Day04/PassChecker.cs
+++ AoC17/Day04/PassChecker.cs
@@ -5,11 +5,27 @@ namespace AoC17.Day04
     internal class PassChecker
     {
         List<List<string>> phrases = new();
-
         public void ParseInput(List<string> lines)
-            => lines.ForEach(line => phrases.Add(line.Split(" ").ToList() ) );
+            => lines.ForEach(line => phrases.Add(line.Split(" ").ToList()));
+
+        bool CheckPolicy(List<string> wordGroup)
+        {
+            if (wordGroup.Distinct().Count() != wordGroup.Count)
+                return false;
+
+            foreach (string s in wordGroup)
+                foreach (string comp in wordGroup.Where(x => x != s))
+                {
+                    var sorted1 = string.Join("", s.OrderBy(x => x).ToArray());
+                    var sorted2 = string.Join("", comp.OrderBy(x => x).ToArray());
+
+                    if (sorted2 == sorted1)
+                        return false;
+                }
+            return true;
+        }
 
         public int Solve(int part = 1)
-            => phrases.Count(x => x.Distinct().Count() == x.Count);
+            => (part == 1) ? phrases.Count(x => x.Distinct().Count() == x.Count) : phrases.Count(x => CheckPolicy(x));
     }
 }
