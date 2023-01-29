91264c7533bfef77c6fbf901ddfe4948cf1f390e
silentDoc <ramon78t@yahoo.es>
2023-01-29 22:11:38 +0100

    Day 9 - Part 1 Completed. String checker

---
+++ AoC17/Day09/StringStreamChecker.cs
@@ -0,0 +1,44 @@
+﻿namespace AoC17.Day09
+{
+    internal class StringStreamChecker
+    {
+        string text = "";
+        public void ParseInput(List<string> lines)
+            => text = lines[0];
+
+        int CheckText(int part = 1)
+        {
+            var score = 0;
+            var nestLevel = 0;
+            bool inGarbage = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (inGarbage && text[i] == '!')
+                {
+                    i++;
+                    continue;
+                }
+                
+                if (inGarbage && text[i] != '>')
+                    continue;
+
+                if (inGarbage && text[i] == '>')
+                    inGarbage = false;
+                
+                if (!inGarbage && text[i] == '<')
+                    inGarbage = true;
+                if (text[i] == '{')
+                    nestLevel++;
+                if (text[i] == '}')
+                {
+                    score += nestLevel;
+                    nestLevel--;
+                }
+            }
+            return score;
+        }
+
+        public int Solve(int part = 1)
+            => CheckText(part);
+    }
+}
--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -8,7 +8,7 @@ namespace AoC17
         {
             int day = 9;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -109,7 +109,9 @@ namespace AoC17
         static int day9(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return 0;
+            Day09.StringStreamChecker checker = new();
+            checker.ParseInput(lines);
+            return checker.Solve(part);
         }
 
     }
