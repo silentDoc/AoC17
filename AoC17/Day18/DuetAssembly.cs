62af0a96c26b46f3a67417d98f126a531be069d4
silentDoc <ramon78t@yahoo.es>
2023-02-05 19:50:10 +0100

    Day 18 - Part 2 Completed. Sincronized programs

--- AoC17/Day18/DuetAssembly.cs
+++ AoC17/Day18/DuetAssembly.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace AoC17.Day18
 {
@@ -16,17 +17,28 @@ namespace AoC17.Day18
                 retVal = registers[op];
             return retVal;
         }
-       
-        public (int newIndex, long newValue) Run(Dictionary<string, long> registers)
+
+        long sendToOtherProgram(Queue<long>? otherProgramQueue, long value)
+        {
+            otherProgramQueue.Enqueue(value);
+            return value;
+        }
+
+        public (int newIndex, long newValue) Run(Dictionary<string, long> registers, int part = 1,
+                                                 Queue<long>? myQueue = null, Queue<long>? otherProgramQueue = null)
             => command switch
             {
-                "snd" => (index + 1, registers["lastSound"] = getValue(op1, registers)),
+                "snd" => part == 1
+                                ? (index + 1, registers["lastSound"] = getValue(op1, registers))
+                                : (index + 1, sendToOtherProgram(otherProgramQueue, getValue(op1, registers))),
                 "set" => (index + 1, registers[op1] = getValue(op2, registers)),
                 "add" => (index + 1, registers[op1] += getValue(op2, registers)),
                 "mul" => (index + 1, registers[op1] *= getValue(op2, registers)),
                 "mod" => (index + 1, registers[op1] %= getValue(op2, registers)),
-                "rcv" => (index + 1, registers[op1]!=0 ? registers[op1] = registers["received"] = registers["lastSound"] : -1),
-                "jgz" => ((getValue(op1, registers) > 0) ? index + (int) getValue(op2, registers) : index + 1, (long) 0),
+                "rcv" => part == 1
+                                ? (index + 1, registers[op1] != 0 ? registers[op1] = registers["received"] = registers["lastSound"] : -1)
+                                : myQueue.Count > 0 ? (index + 1, registers[op1] = myQueue.Dequeue()) : (index, -1234567),
+                "jgz" => ((getValue(op1, registers) > 0) ? index + (int)getValue(op2, registers) : index + 1, (long)0),
                 _ => throw new Exception("Invalid command")
             };
 
@@ -45,18 +57,18 @@ namespace AoC17.Day18
 
     internal class DuetAssembly
     {
-        Dictionary<string, long> registers = new();
         List<Instruction> program = new();
 
         public void ParseInput(List<string> lines)
-        {
+        { 
             for (int row = 0; row < lines.Count; row++)
                 program.Add(new Instruction(lines[row], row));
         }
 
-        long RunProgram(int part = 1)
+        long RunProgram()
         {
             int currentIndex = 0;
+            Dictionary<string, long> registers = new();
             for (char c = 'a'; c < 'z'; c++)    
                 registers[c.ToString()] = 0;
 
@@ -67,12 +79,43 @@ namespace AoC17.Day18
                 if (registers.ContainsKey("received"))
                     return registers["received"];
             }
-
             return -1;
         }
 
+        long RunPrograms()
+        {
+            Dictionary<string, long> registers_0 = new();
+            Dictionary<string, long> registers_1 = new();
+            Queue<long> received_0 = new();
+            Queue<long> received_1 = new();
+            int currentIndex_0 = 0;
+            int currentIndex_1 = 0;
+            long retVal1 = 0;
+            long retVal2 = 0;
+            bool inDeadlock = false;
+
+            for (char c = 'a'; c < 'z'; c++)
+                registers_0[c.ToString()] = registers_1[c.ToString()] = 0;
+
+            registers_0["p"] = 0;
+            registers_1["p"] = 1;
+
+            int numProgram1Sends = 0;
+
+            while (currentIndex_0<program.Count && currentIndex_1<program.Count && !inDeadlock)
+            {
+                (currentIndex_0, retVal1) = program[currentIndex_0].Run(registers_0, 2, received_0, received_1);
+                numProgram1Sends += program[currentIndex_1].command == "snd" ? 1 : 0;
+                (currentIndex_1, retVal2) = program[currentIndex_1].Run(registers_1, 2, received_1, received_0);
+
+                inDeadlock = retVal1 == -1234567 && retVal2 == -1234567;
+            }
+            return numProgram1Sends;
+
+        }
+
         public long Solve(int part = 1)
-            => RunProgram(part);
+            => part == 1 ? RunProgram() : RunPrograms();
     }
 }
 
