74f0a10d88bfecb4d09449094201a2d3781f8475
silentDoc <ramon78t@yahoo.es>
2023-02-05 09:42:23 +0100

    Day 17 - Part 1 Completed. Circular buffer insertions

---
+++ AoC17/Day17/Spinlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC17.Day17
+{
+    internal class Spinlock
+    {
+        List<int> circularBuffer = new();
+        int skip = 0;
+
+        public void ParseInput(List<string> lines)
+            => skip = int.Parse(lines[0]);
+
+        int InsertValues()
+        {
+            circularBuffer.Add(0);
+            int currentPosition = 0;
+            for (int currentValue = 1; currentValue <= 2017; currentValue++)
+            {
+                currentPosition += skip +1;
+                currentPosition %= circularBuffer.Count;
+                circularBuffer.Insert(currentPosition, currentValue);
+            }
+            var pos2017 = circularBuffer.IndexOf(2017);
+            return circularBuffer[(pos2017 +1) % circularBuffer.Count];
+        }
+
+        public int Solve(int part = 1)
+            => InsertValues();
+    }
+}
--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -8,7 +8,7 @@ namespace AoC17
         {
             int day = 17;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -181,7 +181,9 @@ namespace AoC17
         static int day17(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return 0;
+            Day17.Spinlock spin = new();
+            spin.ParseInput(lines);
+            return spin.Solve(part);
         }
     }
 }
