d3a7508a47c251984a3a05499e0747dee893267f
silentDoc <ramon78t@yahoo.es>
2023-02-05 09:58:17 +0100

    Day 17 - Part 2 Completed. Finding the element after zero

--- AoC17/Day17/Spinlock.cs
+++ AoC17/Day17/Spinlock.cs
@@ -28,7 +28,29 @@ namespace AoC17.Day17
             return circularBuffer[(pos2017 +1) % circularBuffer.Count];
         }
 
+        int FindValueAfterZero()
+        {
+            int currentPosition = 0;
+            int positionOfZero = 0;
+            int elementAfterZero = 0;
+            int elementCount = 1;
+
+            for (int currentValue = 1; currentValue <= 50000000; currentValue++)
+            {
+                currentPosition += skip;
+                currentPosition %= elementCount;
+                currentPosition++;
+
+                if (currentPosition <= positionOfZero)
+                    positionOfZero++;
+                if (currentPosition == (positionOfZero + 1) % elementCount)
+                    elementAfterZero = currentValue;
+                elementCount++;
+            }
+            return elementAfterZero;
+        }
+
         public int Solve(int part = 1)
-            => InsertValues();
+            => part ==1 ? InsertValues() : FindValueAfterZero();
     }
 }
