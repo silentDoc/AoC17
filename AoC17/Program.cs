3d03e110399edd290d234f78745727ea17619fe7
silentDoc <ramon78t@yahoo.es>
2023-02-07 08:59:17 +0100

    Day 22 - Prepare Readme, Input and Main

--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -6,9 +6,9 @@ namespace AoC17
     {
         static void Main(string[] args)
         {
-            int day = 21;
-            int part = 2;
-            bool test = false;
+            int day = 22;
+            int part = 1;
+            bool test = !false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -39,6 +39,7 @@ namespace AoC17
                 19 => day19(input, part).ToString(),
                 20 => day20(input, part).ToString(),
                 21 => day21(input, part).ToString(),
+                22 => day22(input, part).ToString(),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -221,6 +222,12 @@ namespace AoC17
             fractalGenerator.ParseInput(lines);
 
             return fractalGenerator.Solve(part);
+        }
+
+        static int day22(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return 0;
         }
     }
 }
